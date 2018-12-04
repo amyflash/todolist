f4b9a35aa6c95071184694582cbbb5399e0b5741
amyflash <flashaq@hotmail.com>
2018-12-04 15:32:20 +0800

    v1.0.0

---
+++ db/config.php
@@ -0,0 +1,26 @@
+<?php
+$config = array();
+$config['db'] = 'mysql';
+$config['mysql.host'] = 'localhost';
+$config['mysql.db'] = 'wpblog';
+$config['mysql.user'] = 'root';
+$config['mysql.password'] = 'your pwd';
+$config['prefix'] = 'mtt_';
+$config['url'] = '';
+$config['mtt_url'] = '';
+$config['title'] = 'todo';
+$config['lang'] = 'cn';
+$config['password'] = 'your pwd';
+$config['smartsyntax'] = 1;
+$config['timezone'] = 'Asia/Shanghai';
+$config['autotag'] = 1;
+$config['duedateformat'] = 1;
+$config['firstdayofweek'] = 1;
+$config['session'] = 'files';
+$config['clock'] = 12;
+$config['dateformat'] = 'Y/n/j';
+$config['dateformat2'] = 'y/n/j';
+$config['dateformatshort'] = 'm.d';
+$config['template'] = 'default';
+$config['showdate'] = 1;
+?>
